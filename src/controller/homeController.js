3b23743af9137b9d42ac248064d84834439d9dc7
Your Name <you@example.com>
2024-07-01 20:39:29 +0700

    hashpassword

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,11 +1,4 @@
-import mysql from 'mysql2';
-
-// Create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'jwt'
-});
+import userService from '../service/useService'
 
 const handleHelloWorld = (req, res) => {
     return res.render("home.ejs")
@@ -20,15 +13,9 @@ const handleCreateNewUser = (req, res) => {
     let password = req.body.password;
     let username = req.body.username;
 
-    connection.query(
-        'INSERT INTO users (email, password, username) VALUES (?, ?, ?)', [email, password, username],
-        function(err, results, fields) {
-            if (err) {
-                console.log(err)
-            }
-        }
-    );
-    
+    // userService.createNewUser(email, password, username)
+    userService.getUserList(email, password, username)
+
     return res.send("hi")
 }
 
