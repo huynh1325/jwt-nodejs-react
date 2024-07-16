e6028c9c396166c8319311aaec2a974f2d0c649e
Your Name <you@example.com>
2024-07-16 16:16:08 +0700

    axios post data

---
+++ src/controller/apiController.js
@@ -0,0 +1,14 @@
+const testApi = (req,res) => {
+    return res.status(200).json({
+        message: 'oke',
+        data: 'test api'
+    })
+}
+
+const handleRegister = (req, res) => {
+    console.log("call me", req.body)
+}
+
+module.exports = {
+    testApi, handleRegister
+}
--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from "express"
 import configViewEngine from "./config/viewEngine"
 import initWebRoutes from "./routes/web"
+import initApiRoutes from "./routes/api"
+import configCors from "./config/cors"
 require("dotenv").config();
 import bodyParser from "body-parser";
 // import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080
+
+configCors(app)
 
 //config view engine
 configViewEngine(app)
@@ -19,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // connection();
 
 //init web routes
-initWebRoutes(app)
+initWebRoutes(app);
+initApiRoutes(app);
 
 app.listen(PORT, () => {
     console.log("JWT Backend is running on the port = " + PORT)
