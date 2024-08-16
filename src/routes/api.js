c02f6eb08d460bcfa1094b590bc1e69220e0ba64
huynh <huynhnguyen120202@gmail.com>
2024-08-16 11:05:37 +0700

    11.6 JWT Middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from "express"
 import apiController from '../controller/apiController'
 import userController from '../controller/userController'
 import groupController from '../controller/groupController'
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTAction'
 
 const router = express.Router();
 
@@ -10,19 +11,35 @@ const router = express.Router();
  * @param {*} app :express app
  */
 
-    const initApiRoutes = (app) => {
-        router.get("/test-api", apiController.testApi);
-        router.post("/register", apiController.handleRegister);
-        router.post("/login", apiController.handleLogin);
+const testMiddleware = (req, res, next) => {
+    console.log("calling a middleware");
+    if (true) {
+        return res.send("reject middleware");
+    }
+}
 
-        router.get("/user/read", userController.readFunc);
-        router.post("/user/create", userController.createFunc);
-        router.put("/user/update", userController.updateFunc);
-        router.delete("/user/delete", userController.deleteFunc);
+// const checkUserLogin = (req, res, next) => {
+//     const nonSecurePaths = ['/', '/register', '/login'];
+//     if (nonSecurePaths.includes(req.path)) return next();
+//     if (user) {
+//         next();
+//     } else {
 
-        router.get("/group/read", groupController.readFunc);
+//     }
+// }
 
-        return app.use("/api/v1/", router);
+const initApiRoutes = (app) => {
+    router.post("/register", apiController.handleRegister);
+    router.post("/login", apiController.handleLogin);
+
+    router.get("/user/read", checkUserJWT, checkUserPermission, userController.readFunc);
+    router.post("/user/create", userController.createFunc);
+    router.put("/user/update", userController.updateFunc);
+    router.delete("/user/delete", userController.deleteFunc);
+
+    router.get("/group/read", groupController.readFunc);
+
+    return app.use("/api/v1/", router);
 }
 
 export default initApiRoutes;
